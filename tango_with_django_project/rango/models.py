7a1ed48e9e0caf74fd89d730b2e63f9179d2b824
pete <nishizawa23@gmail.com>
2017-07-09 01:04:25 +0800

    Configuring the Admin Interface

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -4,6 +4,10 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     def __str__(self):  # For Python 2, use __unicode__ too
         return self.name
 
@@ -13,5 +17,6 @@ class Page(models.Model):
     title = models.CharField(max_length=128)
     url = models.URLField()
     views = models.IntegerField(default=0)
+
     def __str__(self):  # For Python 2, use __unicode__ too
         return self.title
