f694197a1d3ae4c94f85c13bad3105db21f176b1
pete <nishizawa23@gmail.com>
2017-07-09 21:56:45 +0800

    Update modle for admin page

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -4,6 +4,8 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    view = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
 
     class Meta:
         verbose_name_plural = 'Categories'
